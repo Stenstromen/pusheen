0466285372263aebc83feaecebb86f45f0f17ef4
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-06-10 14:16:02 +0200

    Added image preloading

--- src/components/ImageBody.jsx
+++ src/components/ImageBody.jsx
@@ -1,10 +1,18 @@
+import { Helmet } from "react-helmet";
+
 function ImageBody({ image, isMobile }) {
-    return (
-      <div>
-        <img width={isMobile ? "390px" : "800px"} src={`${image}.webp`} alt={`fam/${image}.webp`}/>
-      </div>
-    );
-  }
-  
-  export default ImageBody;
-  
+  return (
+    <div>
+      <Helmet>
+        <link rel="preload" as="image" href={`${image}.webp`} />
+      </Helmet>
+      <img
+        width={isMobile ? "390px" : "800px"}
+        src={`${image}.webp`}
+        alt={`fam/${image}.webp`}
+      />
+    </div>
+  );
+}
+
+export default ImageBody;
